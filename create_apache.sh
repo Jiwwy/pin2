845c6b40858a13f2fecfb25408a8343db8ed46ef
Jimmy <jiwwy_djfercho@hotmail.com>
2025-02-02 16:10:11 -0500

    Update create_apache.sh

--- create_apache.sh
+++ create_apache.sh
@@ -2,4 +2,4 @@
 sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
-echo "<h1> PIN2 Jimmy Faican </h1>" > /var/www/html/index.html
+echo "<h1> PIN2 Jimmy Faican 02022025 </h1>" > /var/www/html/index.html
