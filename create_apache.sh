72c14887077e5938812b7325eda1adfc39299935
Jimmy <jiwwy_djfercho@hotmail.com>
2025-04-10 21:14:09 -0500

    Update create_apache.sh

--- create_apache.sh
+++ create_apache.sh
@@ -1,28 +1,16 @@
-  #! /bin/bash
-  # Actualizar el sistema
-  sudo apt update -y
-  sudo apt upgrade -y
+#!/bin/bash
 
-  # Instalar dependencias necesarias
-  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  # Actualizar los paquetes existentes
+  sudo yum update -y
 
-  # Agregar la clave GPG oficial de Docker
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-  # Agregar el repositorio de Docker
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-  # Actualizar los repositorios
-  sudo apt update -y
-
-  # Instalar Docker CE
-  sudo apt install docker-ce -y
+  # Instalar Docker usando amazon-linux-extras
+  sudo amazon-linux-extras install docker -y
 
   # Iniciar el servicio Docker
-  sudo systemctl start docker
+  sudo service docker start
 
   # Habilitar Docker para que inicie con el sistema
   sudo systemctl enable docker
 
-  # Agregar el usuario al grupo docker
-  sudo usermod -aG docker $(whoami)
+  # Agregar el usuario ec2-user al grupo docker
+  sudo usermod -aG docker ec2-user
