577a8e9d2700dfa71521d0c715cefa9c4981277c
Jimmy <jiwwy_djfercho@hotmail.com>
2025-04-10 21:24:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ##Create and bootstrap webserver #lanzar
 resource "aws_instance" "webserver" {
-  ami                         = "ami-00c39f71452c08778"
+  ami                         = "ami-084568db4383264d4"
   instance_type               = "t2.micro"
   #key_name                    = app-ssh-key 
   associate_public_ip_address = true
